0c42e2d1b3b1347ca55a983deda0453e4caefa8b
Jianbo He <hjbxyz@gmail.com>
2020-01-08 17:44:55 +0800

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-sed -i '0,/'"$1"'/s//'"$2"'/' \
-install.sh \
-rtm-gwas-snpldb/src/snpldb.cpp \
-rtm-gwas-gsc/src/gsc.cpp \
-rtm-gwas-assoc/src/assoc.cpp \
+a=(
+install.sh
+rtm-gwas-snpldb/src/snpldb.cpp
+rtm-gwas-gsc/src/gsc.cpp
+rtm-gwas-assoc/src/assoc.cpp
 rtm-gwas-gui/src/mainwindow.cpp
+)
+
+for e in ${a[@]}
+do
+    sed -i '0,/'"$1"'/s//'"$2"'/' $e
+done
+
+if [[ $(uname) == *"MINGW"* || $(uname) == *"MSYS"* ]]; then
+    for e in ${a[@]}
+    do
+        sed -i 's/$/\r/' $e
+    done
+fi
